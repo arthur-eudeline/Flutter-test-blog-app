1a4430f632acecd777d87392fdbea08acbb3fb86
Arthur Eudeline <arthur@originis.fr>
2019-01-23 13:36:36 +0100

    CustomMenu Widget Creation

--- lib/CustomAppBar.dart
+++ lib/CustomAppBar.dart
@@ -1,27 +1,24 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget{
-  
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Size get preferredSize => new Size.fromHeight(kToolbarHeight);
-  
+
   CustomAppBar();
-  
+
   @override
-  AppBar build(BuildContext context){
+  AppBar build(BuildContext context) {
     return AppBar(
       title: Text("Actualités"),
       centerTitle: true,
-      leading: IconButton(
-        icon: Icon(Icons.menu),
-        color: Colors.white,
-        disabledColor: Color.fromARGB(100, 255, 255, 255),
-        tooltip: "Menu",
-        onPressed: () {
-          Scaffold.of(context).openDrawer();
-        },
+      leading: Builder(
+        builder: (context) => IconButton(
+              icon: new Icon(Icons.menu),
+              color: Colors.white,
+              disabledColor: Color.fromRGBO(255, 255, 255, 0.7),
+              onPressed: () => Scaffold.of(context).openDrawer(),
+            ),
       ),
     );
   }
-  
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+
 import 'CustomAppBar.dart';
+import 'CustomMenu.dart';
 
 void main() => runApp(new MyApp());
 
@@ -10,68 +12,13 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
         title: 'Welcome to Flutter',
         home: Scaffold(
+          
           // Top bar
           appBar: CustomAppBar(),
           // /Top bar
           
           // Menu
-          drawer: Drawer(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  DrawerHeader(
-                      decoration: new BoxDecoration(
-                        image: new DecorationImage(
-                          image: new NetworkImage(
-                              'http://mooxidesign.com/wp-content/uploads/2015/11/material-background.png'),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Container(
-                                width: 60.0,
-                                height: 60.0,
-                                child: ClipOval(
-                                    child: Image(
-                                        image: NetworkImage(
-                                            "https://scontent-cdt1-1.xx.fbcdn.net/v/t31.0-8/17039357_1293112047445505_5669533624310802362_o.jpg?_nc_cat=109&_nc_ht=scontent-cdt1-1.xx&oh=e640e6efb376d92ced387d903f8c2c9a&oe=5CFAEC99"),
-                                        fit: BoxFit.cover)),
-                              )
-                            ],
-                          ),
-                          Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Text("Arthur Eudeline",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Colors.white)),
-                                    Text("Full-Stack Developer",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.w400,
-                                            color:
-                                            Color.fromRGBO(255, 255, 255, 0.5)))
-                                  ],
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.arrow_drop_down),
-                                  color: Colors.white,
-                                  onPressed: () {},
-                                )
-                              ])
-                        ],
-                      ))
-                ],
-              )),
+          drawer: CustomMenu()
           // / Menu
         ));
   }
